347dc9983cd6a575b103871d8cbfaf4cf9b50850
GijsV05 <gijs.veerman@student.hu.nl>
2023-05-21 15:10:55 +0200

    Eerste paar functies voor Pallet af

---
+++ warehouse/src/Pallet.cpp
@@ -0,0 +1,26 @@
+#include "include\Pallet.hpp"
+#include <iostream>
+
+std::string Pallet::getItemName(){
+    return itemName;
+}
+
+int Pallet::getItemCount(){
+    return itemCount;
+}
+
+int getRemainingSpace(){
+    
+}
+
+bool reallocateEmptyPallet(std::string itemName, int itemCapacity){
+
+}
+
+bool takeOne(){
+
+}
+
+bool putOne(){
+
+}
