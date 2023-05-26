0c6503e6e3033fc3e7f28ca107c90ce96d11ebd7
GijsV05 <gijs.veerman@student.hu.nl>
2023-05-26 19:11:24 +0200

    laatste verandering aan shelf

--- warehouse/src/Shelf.cpp
+++ warehouse/src/Shelf.cpp
@@ -1,10 +1,12 @@
 #include "include\Shelf.hpp"
 #include <iostream>
 
+// Maak een shelf met lege pallets aan
 Shelf::Shelf(){
     this->pallets = {Pallet(), Pallet(), Pallet(), Pallet()};
 }
 
+// Ruil pallets van plek
 bool Shelf::swapPallet(int slot, int slot2){
     if(slot <= 4 && slot2 <= 4){
         Pallet pal = pallets[slot];
@@ -16,6 +18,7 @@ bool Shelf::swapPallet(int slot, int slot2){
     }
 }
 
+// Kijk of er een pallet in de shelf leeg is
 bool Shelf::isEmpty(){
     for(Pallet pal : pallets){
         if (pal.getRemainingSpace() != 0 && pal.getItemName() == "" && pal.getItemCount() != 0){
@@ -25,6 +28,7 @@ bool Shelf::isEmpty(){
     return false; 
 }
 
+// Kijk of alle pallets vol zitten
 bool Shelf::isFull(){
     for(Pallet pal : pallets){
         if (pal.getRemainingSpace() == 0 && pal.getItemName() != "" && pal.getItemCount() == 0){
